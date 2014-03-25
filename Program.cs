9b5dabd08c755ed545505e9df769c2e640ad53c0
clement <clemrichard@laposte.net>
2014-03-25 16:51:16 +0100

    new proj

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace loto
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            int[] monTab = new int{6};
+            Console.WriteLine("LE LOTO\n");
+            Console.WriteLine(System.DateTime.Now);
+            Random rand = new Random();
+
+            for (int i = 0; i < 6; i++)
+            {
+                monTab[i] = rand.Next();
+                Console.WriteLine(monTab[i]);
+            }
+            
+
+        }
+    }
+}
